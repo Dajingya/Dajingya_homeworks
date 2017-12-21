325ff2c94e137b025f26f3393712402141db0b44
WangJing <WangJing@192.168.41.2>
2017-12-21 21:04:05 +0800

    提交2017年12月21日作业

--- src/main/java/dt59homework/Demo1.java
+++ src/main/java/dt59homework/Demo1.java
@@ -5,23 +5,22 @@
  * Date:2017年12月20日下午7:11:34
  * Copyright (c) 2017, bluemobi All Rights Reserved.
  *
-*/
+ */
 
 package dt59homework;
+
 /**
- * Description:	   <br/>
- * Date:     2017年12月20日 下午7:11:34 <br/>
- * @author   dingP
- * @version  
- * @see 	 
+ * Description: <br/>
+ * Date: 2017年12月20日 下午7:11:34 <br/>
+ * 
+ * @author dingP
+ * @version
+ * @see
  */
 public class Demo1 {
 
     public static void main(String[] args) {
 
-        //  Auto-generated method stub
-
     }
 
 }
-
